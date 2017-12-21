b7f206db0d39d8d2f00071e9d44aefb85184ecac
Alejandro Sanchez <aalejo@gmail.com>
2017-12-21 20:51:51 +0000

    configuration upgrade

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'My Minimal React Webpack Babel Setup';
+const title = 'My Minimal React-Webpack Setup';
 
 ReactDOM.render(
   <div>{title}</div>,
